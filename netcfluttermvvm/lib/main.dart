b9faef79865dcb74d2928d94a54e23ff28a454b2
pocataylo@xtensa.ph <pocataylo@xtensa.ph>
2025-06-30 16:50:20 +0800

    Tested commit for main dart

--- netcfluttermvvm/lib/main.dart
+++ netcfluttermvvm/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:netcfluttermvvm/views/homepage.dart';
 
 // Make sure the path and filename match the actual location and casing of your HomePage widget file.
 //pleas approve my push
+//this is a new commit
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
